d0c8ebb28c45421fe32622e80c73aacdc0c88307
Larsenv <larsen@weborican.com>
2016-09-17 14:24:09 -0500

    Fixed if statements again

--- Linux64/create-image.sh
+++ Linux64/create-image.sh
@@ -13,37 +13,37 @@ VERSION="$ID$(./version)"
 
 rm header.bin
 
-if [ $VERSION -eq RUUE010 ]
+if [ $VERSION -eq "RUUE010" ]
 then
 	OFFSET="80474CB8"
 	BMG="USA"
 fi
 
-if [ $VERSION -eq RUUE011 ]
+if [ $VERSION -eq "RUUE011" ]
 then
 	OFFSET="80474F38"
 	BMG="USA"
 fi
 
-if [ $VERSION -eq RUUJ011 ]
+if [ $VERSION -eq "RUUJ011" ]
 then
 	OFFSET="80474DE8"
 	BMG="JPN"
 fi
 
-if [ $VERSION -eq RUUK011 ]
+if [ $VERSION -eq "RUUK011" ]
 then
 	OFFSET="8047DE98"
 	BMG="KOR"
 fi
 
-if [ $VERSION -eq RUUP010 ]
+if [ $VERSION -eq "RUUP010" ]
 then
 	OFFSET="804753F8"
 	BMG="EUR"
 fi
 
-if [ $VERSION -eq RUUP011 ]
+if [ $VERSION -eq "RUUP011" ]
 then
 	OFFSET="80475678"
 	BMG="EUR"
@@ -51,7 +51,7 @@ fi
 	
 wit dolpatch accf-data/sys/main.dol $OFFSET=B1E7F92ACACD2034E602C73DF43CCE515731279711F4E105CB7E5E1D69EA2A56170E72E1F81CA9FA9EEFAC654BD6C6DC5E4AD1780C8F774BC3190CB618BA5483757D6734F8A1121B01986C6F28F1FAB6AFD2CFAC4570415F23C9B3D3F489934274CE61A93978E0C90A7584AC43330F73B09A8E3A75344D3C90BE67E70E0C1AB8A1C19396F5373EB92251D15EA13719D973A2E0143292ED28F86B16DC9A96D3F6271EAD8CE0764C7D4F88DBB2FD5E4C5CBC426D89434FA9D04E28EAF6C11E7B4C93A6701D344D891A65CB4C7C74FFC3551C5949A2D0368C6A68B81E8B985EB8665C72C3E6FF98A4B1AE4D5D7910729494A1E6F26D5AB2824262EC0482BDCE324D
 
-if [ $BMG -eq USA ]
+if [ $BMG -eq "USA" ]
 then
 	wbmgt patch accf-data/files/Script/ENG/NPC_SP/NPC_gatekeeperA.bmg --patch replace=BMG/ENG/NPC_gatekeeperA.txt
 	wbmgt patch accf-data/files/Script/ENG/NPC_SP/NPC_periko.bmg --patch replace=BMG/ENG/NPC_periko.txt
@@ -79,7 +79,7 @@ then
 	cp BMG/SPA/script.arc accf-data/files/Script/SPA/script.arc
 fi
 
-if [ $BMG -eq JPN ]
+if [ $BMG -eq "JPN" ]
 then
 	wbmgt patch accf-data/files/Script/JPN/NPC_SP/NPC_gatekeeperA.bmg --patch replace=BMG/JPN/NPC_gatekeeperA.txt
 	wbmgt patch accf-data/files/Script/JPN/NPC_SP/NPC_periko.bmg --patch replace=BMG/JPN/NPC_periko.txt
@@ -91,7 +91,7 @@ then
 	cp BMG/JPN/script.arc accf-data/files/Script/JPN/script.arc
 fi
 
-if [ $BMG -eq KOR ]
+if [ $BMG -eq "KOR" ]
 then
 	wbmgt patch accf-data/files/Script/KOR/NPC_SP/NPC_gatekeeperA.bmg --patch replace=BMG/KOR/NPC_gatekeeperA.txt
 	wbmgt patch accf-data/files/Script/KOR/NPC_SP/NPC_periko.bmg --patch replace=BMG/KOR/NPC_periko.txt
@@ -111,7 +111,7 @@ then
 	cp BMG/JPN/script.arc accf-data/files/Script/JPN/script.arc
 fi
 
-if [ $BMG -eq EUR ]
+if [ $BMG -eq "EUR" ]
 then
 	wbmgt patch accf-data/files/Script/ENG/NPC_SP/NPC_gatekeeperA.bmg --patch replace=BMG/ENG/NPC_gatekeeperA.txt
 	wbmgt patch accf-data/files/Script/ENG/NPC_SP/NPC_periko.bmg --patch replace=BMG/ENG/NPC_periko.txt
