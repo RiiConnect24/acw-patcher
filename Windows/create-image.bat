88794328b905ff17be59b5ed9d368c2b1fe5bd6f
larsenv <larsen@weborican.com>
2017-06-08 12:56:01 -0500

    Ported commands to id8

--- Windows/create-image.bat
+++ Windows/create-image.bat
@@ -8,44 +8,39 @@ echo This also patches Nintendo's DLC key to receive DLC from RiiConnect24. For 
 echo Extracting...
 wit extract . -n RUU.01 --DEST ./accf-data/ --psel=DATA -ovv --wiimmfi
 
-copy /b accf-data\disc\header.bin header.bin
+for /f %%i in ('wit id8 *.wbfs') do set ID=%%i
 
-for /f %%i in ('wit id6 *.wbfs') do set ID=%%i
-for /f %%i in ('version.exe') do set VERSION=%ID%%%i
-
-rm header.bin
-
-if %VERSION%==RUUE010 (
+if %VERSION%==RUUE01 00.00 (
 	set OFFSET=80474CB8
 	set BMG=USA
 	GOTO PATCH
 	)
-if %VERSION%==RUUE011 (
+if %VERSION%==RUUE01 00.01 (
 	set OFFSET=80474F38
 	set BMG=USA
 	GOTO PATCH
 	)
-if %VERSION%==RUUJ011 (
+if %VERSION%==RUUJ01 00.01 (
 	set OFFSET=80474DE8
 	set BMG=JPN
 	GOTO PATCH
 	)
-if %VERSION%==RUUK011 (
+if %VERSION%==RUUK01 00.01 (
 	set OFFSET=8047DE98
 	set BMG=KOR
 	GOTO PATCH
 	)
-if %VERSION%==RUUP010 (
+if %VERSION%==RUUP01 00.00 (
 	set OFFSET=804753F8
 	set BMG=EUR
 	GOTO PATCH
 	)
-if %VERSION%==RUUP011 (
+if %VERSION%==RUUP01 00.01 (
 	set OFFSET=80475678
 	set BMG=EUR
 	GOTO PATCH
 	)
-	
+
 :PATCH
 wit dolpatch accf-data/sys/main.dol %OFFSET%=B1E7F92ACACD2034E602C73DF43CCE515731279711F4E105CB7E5E1D69EA2A56170E72E1F81CA9FA9EEFAC654BD6C6DC5E4AD1780C8F774BC3190CB618BA5483757D6734F8A1121B01986C6F28F1FAB6AFD2CFAC4570415F23C9B3D3F489934274CE61A93978E0C90A7584AC43330F73B09A8E3A75344D3C90BE67E70E0C1AB8A1C19396F5373EB92251D15EA13719D973A2E0143292ED28F86B16DC9A96D3F6271EAD8CE0764C7D4F88DBB2FD5E4C5CBC426D89434FA9D04E28EAF6C11E7B4C93A6701D344D891A65CB4C7C74FFC3551C5949A2D0368C6A68B81E8B985EB8665C72C3E6FF98A4B1AE4D5D7910729494A1E6F26D5AB2824262EC0482BDCE324D
 
@@ -147,7 +142,7 @@ if %BMG%==EUR (
 	)
 
 mkdir wiimmfi-images
-wit copy accf-data wiimmfi-images --update --psel=data --wiimmfi -ovv
+wit copy accf-data wiimmfi-images --update --psel=DATA --wiimmfi -ovv
 wit move wiimmfi-images/accf-data.wbfs wiimmfi-images
 
 rmdir accf-data
